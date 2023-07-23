3d95f0b8864cd64b6c09b78d846386a9e0af65e5
Saboten758 <debjitdaw03.com>
2023-07-23 12:48:18 +0530

    formal

--- screens/Files.tsx
+++ screens/Files.tsx
@@ -1,21 +1,41 @@
 import React, { useState, useEffect } from "react";
 import RNFS from 'react-native-fs';
-import { Text, StyleSheet, View, TouchableOpacity, FlatList } from "react-native";
+import { Text, StyleSheet, View, TouchableOpacity, FlatList, ToastAndroid } from "react-native";
 import { useFocusEffect } from '@react-navigation/native';
-import FileViewer from 'react-native-file-viewer';
 import Share from 'react-native-share';
-
+import DocumentPicker from 'react-native-document-picker'
 const Files = () => {
   const [fileList, setFileList] = useState([]);
 
+  const showInfo=()=>{
+    ToastAndroid.show("All Logged Files Deleted !",ToastAndroid.SHORT);
+  }
 
-
+  const open = async () => {
+    try {
+      const documentsDirectoryPath = RNFS.DocumentDirectoryPath;
+      
+      const result = await DocumentPicker.pick({
+        transitionStyle:'partialCurl',
+        mode:'open',
+        type: [DocumentPicker.types.allFiles],
+      });
+    } catch (error) {
+      if (DocumentPicker.isCancel(error)) {
+        console.log('User cancelled the document picker.');
+      } else {
+        console.log('Error selecting document:', error);
+      }
+    }
+  };
+  
   const del = async () => {
     try {
       const path = RNFS.DocumentDirectoryPath;
       fileList.forEach(async (file) => {
         await RNFS.unlink(`${path}/${file.name}`);
       });
+      showInfo();
       loadFiles(); 
     } catch (error) {
       console.log('Error deleting files:', error);
@@ -50,10 +70,12 @@ const Files = () => {
       console.log('Error reading file:', error);
     }
   };
-  
+
   return (
+    
     <View style={styles.container}>
-      <Text style={{color:'black',marginBottom:10}}>List of Files:</Text>
+      <TouchableOpacity onPress={open} style={styles.head}><Text style={{color:'white'}}> ALL FILES</Text></TouchableOpacity>
+      <Text style={styles.normal}>Share Logged Files:</Text>
       <FlatList
         data={fileList}
         keyExtractor={(item) => item.name}
@@ -63,7 +85,10 @@ const Files = () => {
           </TouchableOpacity>
         )}
       />
-      <TouchableOpacity onPress={del}><Text style={{color:'black'}}> DELETE ALL</Text></TouchableOpacity>
+      <View style={{flexDirection:'row'}}>
+      <TouchableOpacity style={styles.bot}onPress={del}><Text style={{color:'white'}}> DELETE ALL</Text></TouchableOpacity>
+      
+      </View>
     </View>
   );
 };
@@ -80,4 +105,27 @@ const styles = StyleSheet.create({
       color:'black',
       paddingVertical: 8,
     },
+    normal:{
+      color:'black',
+      marginBottom:10
+    },
+    head:{
+      width:100,
+      height:40,
+      marginEnd:10,
+      justifyContent:"center",
+      alignItems:'center',
+      alignSelf:'flex-end',
+      borderRadius:10,
+      backgroundColor:'#666699'
+    },
+    bot:{
+      borderRadius:10,
+      backgroundColor:'#990000',
+      justifyContent:"center",
+      alignItems:'center',
+      width:100,
+      height:40,
+
+    }
 });
